aab6228d996faf4902dcb5d980c85717e0481dcf
Juan-Ignacio-Pacheco-Castro <juan.pachecocastro@ucr.ac.cr>
2021-09-04 16:29:43 -0600

    Punto 5.2

--- Laboratorio4/Laboratorio4/Handlers/PlanetasHandler.cs
+++ Laboratorio4/Laboratorio4/Handlers/PlanetasHandler.cs
@@ -17,5 +17,40 @@ namespace Laboratorio4.Handlers
             rutaConexion = ConfigurationManager.ConnectionStrings["PlanetasConnection"].ToString();
             conexion = new SqlConnection(rutaConexion);
         }
+
+        private DataTable crearTablaConsulta(string consulta)
+        {
+            SqlCommand comandoParaConsulta = new SqlCommand(consulta, conexion);
+            SqlDataAdapter adaptadorParaTabla = new SqlDataAdapter(comandoParaConsulta);
+            DataTable consultaFormatoTabla = new DataTable();
+
+            conexion.Open();
+            adaptadorParaTabla.Fill(consultaFormatoTabla);
+            conexion.Close();
+            return consultaFormatoTabla;
+        }
+
+        public List<PlanetaModel> obtenerTodosLosPlanetas()
+        {
+            List<PlanetaModel> planetas = new List<PlanetaModel>();
+            string consulta = "SELECT * FROM Planeta ";
+            DataTable tablaResultado = crearTablaConsulta(consulta);
+
+            foreach(DataRow columna in tablaResultado.Rows)
+            {
+                planetas.Add(
+                    new PlanetaModel
+                    {
+                        nombre = Convert.ToString(columna["nombrePlaneta"]),
+                        tipo = Convert.ToString(columna["tipoPlaneta"]),
+                        id = Convert.ToInt32(columna["planetaId"]),
+                        numeroAnillos = Convert.ToInt32(columna["numeroAnillos"])
+                    });
+            }
+
+            return planetas;
+        }
+
+
     }
 }
