d0b1d09ff854be45497987dbedb9e827142d9cbb
Juan-Ignacio-Pacheco-Castro <juan.pachecocastro@ucr.ac.cr>
2021-09-04 17:16:44 -0600

    Punto 6

---
+++ Laboratorio4/Laboratorio4/Controllers/PlanetasController.cs
@@ -0,0 +1,18 @@
+﻿using Laboratorio4.Models;
+using Laboratorio4.Handlers;
+using System.Web.Mvc;
+using System;
+
+namespace Laboratorio4.Controllers
+{
+    public class PlanetasController : Controller
+    {
+        // GET: Planetas
+        public ActionResult listadoDePlanetas()
+        {
+            PlanetasHandler accesoDatos = new PlanetasHandler();
+            ViewBag.planetas = accesoDatos.obtenerTodosLosPlanetas();
+            return View();
+        }
+    }
+}
