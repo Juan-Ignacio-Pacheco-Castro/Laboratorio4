7f28f83691e902627964de9a6fceaebd2274fd3a
Juan-Ignacio-Pacheco-Castro <juan.pachecocastro@ucr.ac.cr>
2021-09-04 09:49:16 -0600

    Puntos 2,3,4 del Lab4

---
+++ Laboratorio4/Laboratorio4/Handlers/PlanetasHandler.cs
@@ -0,0 +1,21 @@
+﻿using Laboratorio4.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Laboratorio4.Handlers
+{
+    public class PlanetasHandler
+    {
+        private SqlConnection conexion;
+        private string rutaConexion;
+
+        public PlanetasHandler()
+        {
+            rutaConexion = ConfigurationManager.ConnectionStrings["PlanetasConnection"].ToString();
+            conexion = new SqlConnection(rutaConexion);
+        }
+    }
+}
